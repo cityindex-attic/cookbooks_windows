69e3cee8b3fbea6d2f03c2898b50ad10e11f667d
mikelear <mike.lear@cityindex.com>
2011-08-18 22:54:06 +0200

    Edited cookbooks/ciapi_in_a_box/powershell_providers/iis/updateACBroker.cmd via GitHub

---
+++ cookbooks/ciapi_in_a_box/powershell_providers/iis/updateACBroker.cmd
@@ -0,0 +1,4 @@
+SET activemq_version=ActiveMQ
+svn export --force --username ciapiinaboxinstaller --password xITVsI https://cityindex.unfuddle.com/svn/cityindex_ciapi-in-a-box/ActiveMQ-MessageBus/%activemq_version%.7z ActiveMQMessageBus/%activemq_version%.7z
+7z x ActiveMQMessageBus\%activemq_version%.7z -oActiveMQMessageBus\%activemq_ver
+sion%\
