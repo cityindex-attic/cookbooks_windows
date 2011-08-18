ebfa1100f931409271c6bc47bc192cae05bda34a
mikelear <mike.lear@cityindex.com>
2011-08-18 22:52:01 +0200

    Edited cookbooks/ciapi_in_a_box/powershell_providers/iis/updateREST.cmd via GitHub

--- cookbooks/ciapi_in_a_box/powershell_providers/iis/updateREST.cmd
+++ cookbooks/ciapi_in_a_box/powershell_providers/iis/updateREST.cmd
@@ -1,1 +1,7 @@
-update REST
+SET RESTWebServices_version=RESTWebservices
+svn export --force --username ciapiinaboxinstaller --password xITVsI https://cityindex.unfuddle.com/svn/cityindex_ciapi-in-a-box/RestWebServices/%RESTWebServices_version%.7z RESTWebServices/%RESTWebServices_version%.7z
+7z x RESTWebservices\%RESTWebServices_version%.7z -oRESTWebServices\%RESTWebServices_version%\
+rmdir RESTWebservices\current
+del /Q RESTWebservices\current_is*.*
+mklink /D RESTWebservices\current %RESTWebServices_version%\RESTWebservices
+echo %RESTWebServices_version% > RESTWebServices\current_is_%RESTWebServices_version%.txt
