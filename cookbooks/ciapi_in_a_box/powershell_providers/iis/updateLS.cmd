24f55dc14d5e9240e8f022b18fc2a2f1dcf1dc39
mikelear <mike.lear@cityindex.com>
2011-08-18 22:57:14 +0200

    Edited cookbooks/ciapi_in_a_box/powershell_providers/iis/updateLS.cmd via GitHub

---
+++ cookbooks/ciapi_in_a_box/powershell_providers/iis/updateLS.cmd
@@ -0,0 +1,3 @@
+SET lightstreamer_version=LightStreamerJavaAdapters
+REM svn export --force --username ciapiinaboxinstaller --password xITVsI https://cityindex.unfuddle.com/svn/cityindex_ciapi-in-a-box/LightStreamer/%lightstreamer_version%.7z LightStreamer/%lightstreamer_version%.7z
+7z x Lightstreamer\%lightstreamer_version%.7z -oLightstreamer\%Lightstreamer_version%\
