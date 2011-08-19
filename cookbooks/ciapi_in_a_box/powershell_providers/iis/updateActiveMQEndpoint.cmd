f08de115e4836dfb62c1741477d7fdb807aee747
David Laing <david@davidlaing.com>
2011-08-19 03:40:04 +0200

    Edited cookbooks/ciapi_in_a_box/powershell_providers/iis/updateActiveMQEndpoint.cmd via GitHub

--- cookbooks/ciapi_in_a_box/powershell_providers/iis/updateActiveMQEndpoint.cmd
+++ cookbooks/ciapi_in_a_box/powershell_providers/iis/updateActiveMQEndpoint.cmd
@@ -1,3 +1,3 @@
 SET activemq_version=ActiveMQStreaming
-call svn export --force --username ciapiinaboxinstaller --password xITVsI https://cityindex.unfuddle.com/svn/cityindex_ciapi-in-a-box/ActiveMQ-Endpoint/%activemq_version%.7z ActiveMQ/%activemq_version%.7z
+call svn export --force --username ciapiinaboxinstaller --password xITVsI https://cityindex.unfuddle.com/svn/cityindex_ciapi-in-a-box/ActiveMQ-Endpoint/%activemq_version%.7z ActiveMQ-Endpoint/%activemq_version%.7z
 7z x ActiveMQ-Endpoint\%activemq_version%.7z -oActiveMQ-Endpoint\%activemq_version%\
